7b814f40f7f92c315cd35e4130d573fa21d2cb5c
JeremyKlein21 <77638390+JeremyKlein21@users.noreply.github.com>
2021-11-15 11:02:48 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,25 @@
+import flask
+import lipd
+from flask import Flask
+from flask import send_from_directory
+from flask import request
+from lipd import readLipd
+
+app = Flask(__name__)
+# setting the route pf the server
+@app.route('/', methods = ['POST'])
+def receiveLiPD():
+    # setting the files from the POST request equal to a variable
+    fileReceived = request.files['pond']
+    # saving the file locally in order to read it
+    fileReceived.save('pond.lpd')
+    # reading the locally saved pond file
+    readFile = lipd.readLipd("C:/Users/Golde/PycharmProjects/FlaskLibrary/pond.lpd")
+    # returning the netCDF file from the resulting climate model
+    return send_from_directory("./", "test.nc")
+
+
+# setting the host and port for the server to run on
+app.run(host='127.0.0.1', port=23657)
+
+
